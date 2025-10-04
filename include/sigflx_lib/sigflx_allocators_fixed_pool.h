b702348034f258db37a241566a4c6369f768a1d8
Landau-Randall <1443409559@qq.com>
2025-10-04 12:15:51 +0800

    新增固定内存池类FixedBlockPool及其单例分配器封装，适用于小对象、小结构体的高频分配，一次只能进行一个对象的分配

---
+++ include/sigflx_lib/sigflx_allocators_fixed_pool.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <cstddef>
+#include <cassert>
+#include <cmath>
+#include "sigflx_lib/sigflx_platform_allocator.h"
+
+namespace SignalFlux
+{
+namespace Allocators
+{
+class FixedBlockPool
+{
+public:
+    using SizeType = std::size_t;
+    using ByteType = unsigned char;
+    template<SizeType PoolSize>
+    class PoolBlock
+    {
+        static_assert((PoolSize ^ (PoolSize - 1) == 0) && (PoolSize > 64),"BlockSize must be the power of 2!");
+    public:
+        using SizeType = FixedBlockPool::SizeType;
+        using ByteType = FixedBlockPool::ByteType;
+    private:
+        ByteType * data_ = nullptr;
+        ByteType ** freeList_ = nullptr;
+        SizeType blockSize_ = 0;
+        SizeType blockCount_ = 0;
+        SizeType freeCount_ = 0;
+        bool is2Power(SizeType number) { return (number != 0) && ((number) & (number - 1)) == 0; };
+    public:
+        PoolBlock();
+        PoolBlock(ByteType * source,SizeType blockSize);
+        PoolBlock(const PoolBlock & object) = delete;
+        PoolBlock(PoolBlock && object);
+
+        ~PoolBlock();
+
+        PoolBlock & operator=(const PoolBlock & object) = delete;
+        PoolBlock & operator=(PoolBlock && objcet);
+
+        SizeType blockSize() const noexcept { return blockSize_; };
+        SizeType freeCount() const noexcept { return freeCount_; };
+
+        void * allocateBlock();
+        void deallocateBlock(void * ptr);
+        void release();
+
+        bool isEmpty() const noexcept { return  (blockCount_ != 0) && (freeCount_ == blockCount_); };
+        bool isFree(SizeType n) const noexcept;
+    };
+private:
+    static constexpr SizeType poolSize_ = 2048;
+    ByteType * source_ = nullptr;
+    PoolBlock<poolSize_> pool_[7];
+    SizeType nextPowerOfTwo(SizeType x);
+public:
+    FixedBlockPool();
+    FixedBlockPool(const FixedBlockPool & object) = delete;
+    FixedBlockPool(FixedBlockPool && object) = delete;
+
+    ~FixedBlockPool();
+
+    FixedBlockPool & operator=(const FixedBlockPool & object) = delete;
+    FixedBlockPool & operator=(FixedBlockPool && object) = delete;
+
+
+    template<typename T>
+    T * allocate(SizeType n = 1);
+
+    template<typename T>
+    void deallocate(T * pointer,SizeType n = 1);
+
+    static FixedBlockPool& instance()
+    {
+        static FixedBlockPool pool;
+        return pool;
+    }
+};
+
+
+template<typename T>
+class FixedPoolAllocator
+{
+public:
+    using ValueType = T;
+    using Pointer = T*;
+    using SizeType = std::size_t;
+public:
+    FixedPoolAllocator() noexcept = default;
+    Pointer allocate(SizeType n);
+    void deallocate(Pointer ptr,SizeType n);
+};
+
+}
+}
+
+#include "../../src/sigflx_lib/sigflx_allocators_fixed_pool.tpp"
---
+++ test/sigflx_allocators_fixed_pool_test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include "sigflx_lib/sigflx_allocators_fixed_pool.h"
+
+
+#include <gtest/gtest.h>
+#include "sigflx_lib/sigflx_allocators_fixed_pool.h"
+
+TEST(FixedBlockPoolTest, AllocateAndDeallocateChar)
+{
+    auto& pool = SignalFlux::Allocators::FixedBlockPool::instance();
+    char* p = pool.allocate<char>(1);
+    ASSERT_NE(p, nullptr);
+    pool.deallocate<char>(p, 1);
+}
+
+TEST(FixedBlockPoolTest, AllocateAndDeallocateInt64)
+{
+    auto& pool = SignalFlux::Allocators::FixedBlockPool::instance();
+    int64_t* p = pool.allocate<int64_t>(1);
+    ASSERT_NE(p, nullptr);
+    pool.deallocate<int64_t>(p, 1);
+}
+
+TEST(FixedBlockPoolTest, ReuseBlock)
+{
+    auto& pool = SignalFlux::Allocators::FixedBlockPool::instance();
+    char* p1 = pool.allocate<char>(1);
+    pool.deallocate<char>(p1, 1);
+    char* p2 = pool.allocate<char>(1);
+    ASSERT_EQ(p1, p2); // 复用同一个块
+    pool.deallocate<char>(p2, 1);
+}
