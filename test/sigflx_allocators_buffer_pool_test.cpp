8d4b963d74e6e340df1cf8e54cee0865de6f2c47
Landau-Randall <1443409559@qq.com>
2025-10-04 23:34:12 +0800

    对BufferPool的功能进行测试

---
+++ test/sigflx_allocators_buffer_pool_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "sigflx_lib/sigflx_allocators_buffer_pool.h"
+
+
+SignalFlux::Allocators::BufferPool<float,128,1024> pool;
+
+
+TEST(BufferPoolTest,AllocateAndDeallocate)
+{
+    float * a = pool.allocate(1024);
+    ASSERT_NE(a,nullptr);
+    pool.deallocate(a,1024);
+}
+
+TEST(BufferPoolTest,Reuse)
+{
+    float * p1 = pool.allocate(1024);
+    pool.deallocate(p1,1024);
+    float * p2 = pool.allocate(1024);
+    ASSERT_EQ(p1,p2);
+    pool.deallocate(p2,1024);
+}
