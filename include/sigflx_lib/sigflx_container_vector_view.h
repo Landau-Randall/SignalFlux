da3d487116bdef364aace70dbe70e5d13dd628a8
Landau-Randall <1443409559@qq.com>
2025-10-31 11:49:05 +0800

    新增轻量化矢量视图VectorView类，用于包装数据与尺寸

---
+++ include/sigflx_lib/sigflx_container_vector_view.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <cstddef>
+#include <stdexcept>
+
+
+namespace SignumFlux
+{
+namespace Container
+{
+template<typename T>
+class VectorView
+{
+public:
+    using ValueType = T;
+    using SizeType = std::size_t;
+    using Pointer = const T *;
+    using ConstPointer = const T *;
+    using Iterator = ConstPointer;
+    using ConstIterator = ConstPointer;
+    using Reference = const T &;
+    using ConstReference = const T &;
+private:
+    ConstPointer data_ = nullptr;
+    SizeType size_ = 0;
+public:
+    VectorView() = default;
+    VectorView(const VectorView & object) = default;
+    VectorView(VectorView && object) noexcept = default;
+    VectorView(ConstPointer data,SizeType size);
+
+    ~VectorView() = default;
+
+    VectorView & operator=(const VectorView & object) = default;
+    VectorView & operator=(VectorView && object) = default;
+
+    Pointer data() noexcept { return data_; };
+    ConstPointer data() const noexcept { return data_; };
+    SizeType size() const noexcept { return size_; };
+
+    Reference operator[](SizeType index);
+    ConstReference operator[](SizeType index) const;
+    Reference at(SizeType index);
+    ConstReference at(SizeType index) const;
+
+    Iterator begin();
+    Iterator end();
+    ConstIterator cbegin() const;
+    ConstIterator cend() const;
+
+};
+}
+}
+
+#include "../../src/sigflx_lib/sigflx_container_vector_view.tpp"
